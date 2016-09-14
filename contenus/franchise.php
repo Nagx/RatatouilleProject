57b17511344ac648a9800b582971fd80cc14c1a3
Nagx <k.javazzo@gmail.com>
2016-09-14 13:59:53 +0200

    bug fix

--- contenus/franchise.php
+++ contenus/franchise.php
@@ -13,7 +13,7 @@
 			Enfin un petit rappel de notre devise chez ratatouille vous aurez les 3 B, du Bio, du Beau et du Bon.</p>
 
 			<H2>Emplacement Souhaité</H2>
-<<<<<<< HEAD
+
             <p class="descriptif">Nous souherons un restaurant de 40 places minimum, dans une zone d'activité avec parking, centre commerciaux et centre ville</p>
 
             <H2>Formation et assistance</H2>
@@ -49,69 +49,6 @@
             <label for="InputTelephone">Téléphone</label>
             <input type="telephone" class="form-control" id="InputTelephone" placeholder="Téléphone" required>
         </div>
-=======
-    <p class="descriptif">Nous souherons un restaurant de 40 places minimum, dans une zone d'activité avec parking, centre commerciaux et centre ville.</p>
 
-    <H2>Formation et assistance</H2>
-    <p class="descriptif">Une formation d'un mois sera mise en place à la formation aux bonnes pratiques et à l'utilisation des outils de gestion interne et externe propre à notre franchise.</p>
-</div>
-<div class="form-group">
-    <label for="exampleInputNom">Nom</label>
-    <input type="text" class="form-control" id="InputNom" placeholder="Nom" required>
-</div>
-<div class="form-group">
-    <label for="InputPrenom">Prénom</label>
-    <input type="text" class="form-control" id="Prenom" placeholder="Prénom" required>
-</div>
-<div class="form-group">
-    <label for="Adresse">Adresse</label>
-    <input type="text" class="form-control" id="Adresse" placeholder="Adresse">
-</div>
-<div class="form-group">
-    <label for="CodePostal">Code Postal</label>
-    <input type="text" class="form-control" id="CodePostal" placeholder="Code Postal">
-</div>
-<div class="form-group">
-    <label for="Ville">Ville</label>
-    <input type="text" class="form-control" id="Ville" placeholder="Ville">
-</div>
-<div class="form-group">
-    <label for="InputEmail1">adresse email</label>
-    <input type="email" class="form-control" id="InputEmail1" placeholder="Email">
-</div>
-<div class="form-group">
-    <label for="InputTelephone">Téléphone</label>
-    <input type="telephone" class="form-control" id="InputTelephone" placeholder="Téléphone" required>
-</div>
->>>>>>> origin/Kev
 
-        <div class="form-group">
-            <label for="VilleduResto">Ville du Restaurant</label>
-            <input type="text" class="form-control" id="VilleDuResto" placeholder="Ville du Restaurant" required>
-        </div>
-        <div class="form-group">
-            <label for="AdresseDuResto">Adresse du Restaurant</label>
-            <input type="text" class="form-control" id="AdresseDuResto" placeholder="Adresse du Restaurant">
-        </div>
-        <div class="form-group">
-            <label for="CodePostalDuResto">Code Postal du Restaurant</label>
-            <input type="text" class="form-control" id="CodePostal" placeholder="Code Postal du Restaurant">
-        </div>
-        <div class="form-group">
-            <label for="VilleduResto">Ville du Restaurant</label>
-            <input type="text" class="form-control" id="VilleDuResto" placeholder="Ville du Restaurant">
-        </div>
-        <div class="form-group">
-            <label for="NombrePlace">Nombre de Places</label>
-            <input type="number" class="form-control" id="nbplaceDuResto" placeholder="Nombre de Places">
-        </div>
-        <div class="form-group">
-            <label for="Surface">Surface du Restaurant (en m2)</label>
-            <input type="number" class="form-control" id="surfaceDuResto" placeholder="Surface du Restaurant">
-        </div>
-        <div class="form-group">
-            <label for="commentaires">Commentaires</label>
-            <textarea class="form-control" id="exempleCommentaires" rows="3"></textarea>
-        </div>
     </div>
-</div>
