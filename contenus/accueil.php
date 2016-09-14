bc3e81d7236f74eda2625d74670442f2561eb513
Nagx <k.javazzo@gmail.com>
2016-09-14 10:19:45 +0200

    add option

--- contenus/accueil.php
+++ contenus/accueil.php
@@ -2,3 +2,4 @@
 <p>Nos restaurant sont tous basés sur de la nouriture BIO de première fraicheur.</p>
 <p>Avec des menus propres à chaque restaurant permettant d'adapter le cuisine locale au concept</p>
 <p> le client se retrouve gagnant avec une nourriture saine et de tradition</p>
+<?php include('actualités.php');
