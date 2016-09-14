245d5d839bd814f9c42e2939714eabc5ec23d7ad
dbeauseigneur <dbeauseigneur@gmail.com>
2016-09-14 16:20:08 +0200

    maj du style.css

--- contenus/actualités.php
+++ contenus/actualités.php
@@ -5,12 +5,12 @@
             <h4 class="panel-title">
        <!-- Titre de l'actualité 1 -->
         <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
-        Collapsible Group 1</a>
+        Ouverture d'un nouveau restaurant à Lyon</a>
       </h4>
         </div>
         <div id="collapse1" class="panel-collapse collapse in">
             <!-- Contenu de l'actualité 1 -->
-            <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+            <div class="panel-body">Capitale des gaules et de la gastronomie, Le Ratatouille de Lyon vous attends dès le 5 septembre 2016 pour vous faire redécouvrir la gastronomie lyonnaise à prix attractifs.</div>
         </div>
     </div>
 
@@ -20,12 +20,12 @@
             <h4 class="panel-title">
        <!-- Titre de l'actualité 2 -->
         <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">
-        Collapsible Group 2</a>
+        Ouverture d'un nouveau restaurant à Toulouse</a>
       </h4>
         </div>
         <div id="collapse2" class="panel-collapse collapse">
             <!-- Contenu de l'actualité 2 -->
-            <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+            <div class="panel-body">Venez savourer, dès le 5 septembre 2016, foie gras, cassoulet, saucisses dans une sélection de recettes locales.</div>
         </div>
     </div>
 
@@ -35,12 +35,12 @@
             <h4 class="panel-title">
        <!-- Titre de l'actualité 3 -->
         <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">
-        Collapsible Group 3</a>
+        Ouverture d'un nouveau restaurant à Bordeaux</a>
       </h4>
         </div>
         <div id="collapse3" class="panel-collapse collapse">
             <!-- Contenu de l'actualité 3 -->
-            <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+            <div class="panel-body">Venez savourez autour d'un bon cru ou pas, nos recettes régionales. (Vin à consommer avec modération.)</div>
         </div>
     </div>
 </div>
