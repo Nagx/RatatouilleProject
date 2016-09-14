a1133feac0573eb0d1a4e3be93e49dd0a4581886
dbeauseigneur <dbeauseigneur@gmail.com>
2016-09-14 11:46:54 +0200

    modif sur style.css accueil franchise et qui sommes nous

--- contenus/franchise.php
+++ contenus/franchise.php
@@ -1,15 +1,15 @@
 <div id="franchise_b">
     <H2>Nos franchisés</H1>
-			<p>Nos franchisés sont les représentants de notre marque de fabrique et par conséquent un standing de base est nécessaire.</p>
-			<p>Ils doivent donc proposer un menu avec des produits bio et frais et issue d'une fillière locale.</p>
-			<p>Pour cela les premiers temps la qualité des menus sera vérifié ainsi que la propreté des cuisines.</p>
-			<p>Enfin un petit rappel de notre devise chez ratatouille vous aurez les 3 B, du Bio, du Beau et du Bon</p>
+			<p class="descriptif">Nos franchisés sont les représentants de notre marque de fabrique et par conséquent un standing de base est nécessaire.
+			Ils doivent donc proposer un menu avec des produits bio et frais et issue d'une fillière locale.
+			Pour cela les premiers temps la qualité des menus sera vérifié ainsi que la propreté des cuisines.
+			Enfin un petit rappel de notre devise chez ratatouille vous aurez les 3 B, du Bio, du Beau et du Bon</p>
 
 			<H2>Emplacement Souhaité</H2>
-    <p>Nous souherons un restaurant de 40 places minimum, dans une zone d'activité avec parking, centre commerciaux et centre ville</p>
+    <p class="descriptif">Nous souherons un restaurant de 40 places minimum, dans une zone d'activité avec parking, centre commerciaux et centre ville</p>
 
     <H2>Formation et assistance</H2>
-    <p>Une formation d\'un mois sera mise en place à la formation aux bonnes pratiques et à l'utilisation des outils de gestion interne et externe propre à notre franchise'</p>
+    <p class="descriptif">Une formation d'un mois sera mise en place à la formation aux bonnes pratiques et à l'utilisation des outils de gestion interne et externe propre à notre franchise</p>
 </div>
 <div class="form-group">
     <label for="exampleInputNom">Nom</label>
@@ -42,7 +42,7 @@
 
 <div class="form-group">
     <label for="VilleduResto">Ville du Restaurant</label>
-    <input type="text" class="form-control" id="VilleDuResto" placeholder="Ville du Restaurant">
+    <input type="text" class="form-control" id="VilleDuResto" placeholder="Ville du Restaurant" required>
 </div>
 <div class="form-group">
     <label for="AdresseDuResto">Adresse du Restaurant</label>
