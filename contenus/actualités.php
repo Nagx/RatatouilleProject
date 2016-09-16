66609c03e795c8a86953350b3f543571ecc9cade
Nagx <k.javazzo@gmail.com>
2016-09-16 08:59:43 +0200

    actu lorem ipsum

--- contenus/actualités.php
+++ contenus/actualités.php
@@ -8,6 +8,7 @@ function titre1() { ?>
 // Contenu actu 1
 function content1() { ?> 
     Nous avons le plaisirs d''annoncer l'ouvertures le 5 septembre de 3 nouveaux restaurant à Lyon Toulouse et Bordeaux.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo sapiente assumenda sint aperiam laboriosam vel ratione modi, ad veniam nulla totam laudantium quaerat quasi distinctio autem adipisci inventore voluptatibus? Animi!
 <?php };
 
 
@@ -19,6 +20,7 @@ function titre2() { ?>
 // Contenu actu 2
 function content2() { ?>
    La qualité de réalisation de notre ratatouille, nous as permis d'obtenir le prix de la meilleure ratatouille de la ville de la Loupe.
+   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad provident repudiandae odio, error impedit sapiente aliquid amet sit vero excepturi, aperiam quos exercitationem consequatur ex, tempora eaque eos aspernatur consequuntur.
 <?php };
 
 
@@ -30,6 +32,7 @@ function titre3() { ?>
 // Contenu actu 3
 function content3() { ?>
    La franchise ratatouille a obtenu, le 6 juin dernier, le prix 2016 de la franchise la plus prometteuse du CCI d'Eure-et-Loire.
+   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe architecto, voluptates quam nam! At aspernatur sed nesciunt accusamus accusantium deserunt eveniet, assumenda, eos facilis nihil, quas, praesentium iure. Molestiae, sint.
 <?php };
 
 ?>
